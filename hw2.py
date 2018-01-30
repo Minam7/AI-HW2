47c798ea21da7fe7646c83cfee2a448caf137e2a
mina <mina.m777@yahoo.com>
2018-01-30 11:32:28 +0330

    learning lda model

--- hw2.py
+++ hw2.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
 
 import pickle
-import random
+# import random
 import time
-import sys
+# import sys
 
-import gensim
-import hazm
+# import gensim
+# import hazm
 from sklearn import svm
 
 dic_data = dict()
@@ -19,6 +19,7 @@ class SupervisedData:
         self.topics = None
 
 
+'''
 def stem_data(dat):
     normalizer = hazm.Normalizer()
     dat = normalizer.normalize(dat)
@@ -44,7 +45,7 @@ def stem_data(dat):
                 words.append(stemmed)
 
     return words
-
+'''
 
 if __name__ == '__main__':
 
@@ -58,6 +59,7 @@ if __name__ == '__main__':
     # data_addr = sys.argv[1]
     # label_addr = sys.argv[2]
 
+    '''
     # comment this part if you have saved objects
     tagger = hazm.POSTagger(model='resources/postagger.model')
     stop_words = open('files/stopwords-fa.txt', 'r', encoding='utf-8').read().split('\n')
@@ -96,6 +98,15 @@ if __name__ == '__main__':
     ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=100, id2word=dictionary, passes=50)
     ldamodel.save('files/lda.model')
 
+    # load saved objects
+    ldamodel = gensim.models.LdaModel.load('files/lda.model')
+    dictionary = gensim.corpora.Dictionary.load('files/lda_dictionary.dict')
+    pickle_in = open("files/datas.pickle", "rb")
+    datas = pickle.load(pickle_in)
+
+    pickle_in = open("files/docs_words.pickle", "rb")
+    docs_words = pickle.load(pickle_in)
+
     for i in range(DATA_SIZE + 1):
         tpcs = [0 for x in range(100)]
         for item in ldamodel.get_document_topics(dictionary.doc2bow(docs_words[i])):
@@ -104,34 +115,22 @@ if __name__ == '__main__':
 
     # shuffle data for picking train, validation and test data
     random.shuffle(datas)
+    
+    
+    # saving objects
+    pickle_out = open("files/random_datas.pickle", "wb")
+    pickle.dump(datas, pickle_out)
+    pickle_out.close()
+    '''
+
+    pickle_in = open("files/random_datas.pickle", "rb")
+    datas = pickle.load(pickle_in)
 
     train_set = datas[:TRAIN_SIZE]
     validation_set = datas[TRAIN_SIZE + 1:TRAIN_SIZE + VALIDATION_SIZE + 1]
     test_set = datas[TRAIN_SIZE + VALIDATION_SIZE + 1:]
 
-    # saving objects
-    pickle_out = open("files/train_set.pickle", "wb")
-    pickle.dump(train_set, pickle_out)
-    pickle_out.close()
-
-    pickle_out = open("files/validation_set.pickle", "wb")
-    pickle.dump(validation_set, pickle_out)
-    pickle_out.close()
-
-    pickle_out = open("files/test_set.pickle", "wb")
-    pickle.dump(test_set, pickle_out)
-    pickle_out.close()
-
     # comment part above after saving objects for model learning and division
-    # loading objects
-    pickle_in = open("files/train_set.pickle", "rb")
-    train_set = pickle.load(pickle_in)
-
-    pickle_in = open("files/validation_set.pickle", "rb")
-    validation_set = pickle.load(pickle_in)
-
-    pickle_in = open("files/test_set.pickle", "rb")
-    test_set = pickle.load(pickle_in)
 
     # SVM chapter
 
